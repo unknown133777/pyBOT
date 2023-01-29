0e95305c7fd3eeba6d4ae425c5f61df245d9411f
Lee-d-g2222 <roqkf4641@gmail.com>
2023-01-30 04:27:53 +0900

    message

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import json, os
-import nextcord
-
+import nextcord, os
+import utils.utils as utils
 from nextcord.ext import commands
 
-JSON_PATH = str(os.getcwd()) + '\pyBOT\config.json'
-with open(JSON_PATH, 'r') as file:
-    data = json.load(file)
-
-GUILD_ID = data['config']['guild_id']
-TOKEN = data['config']['token']
+#Read json(config.json)
+data = utils.read_json()
+GUILD_ID = int(data[0])
+TOKEN = data[1]
 
 intents = nextcord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.voice_states = True
+
 bot = commands.Bot()
+
+for file in os.listdir('cogs'):
+    if file.endswith('.py'):
+        bot.load_extension(f'cogs.{file[:-3]}')
 
 bot.run(TOKEN)
